5c0730627aa1d76c6a720b2373f1c9cd09688d7e
Eric Hanson <ehanson@my.yorku.ca>
2026-02-23 01:32:39 -0500

    style: Fix indentation in `OneTimeEvent` and reorder methods for improved consistency

--- server/src/main/java/ca/yorku/eecs2311/schedulelynx/domain/OneTimeEvent.java
+++ server/src/main/java/ca/yorku/eecs2311/schedulelynx/domain/OneTimeEvent.java
@@ -3,23 +3,36 @@ package ca.yorku.eecs2311.schedulelynx.domain;
 import java.time.LocalTime;
 
 public class OneTimeEvent extends TimeBlock {
-  private Long id;
-  private String title;
 
-  public OneTimeEvent() {}
+    private Long id;
+    private String title;
 
-  public OneTimeEvent(Long id, String title, Weekday day, LocalTime start,
-                      LocalTime end) {
-    super(day, start, end);
-    this.id = id;
-    this.title = title;
-  }
+    public OneTimeEvent(
+            Long id,
+            String title,
+            Weekday day,
+            LocalTime start,
+            LocalTime end)
+    {
+        super(day, start, end);
+        this.id = id;
+        this.title = title;
+    }
 
-  public Long getId() { return id; }
+    public Long getId() {
+        return id;
+    }
 
-  public void setId(Long id) { this.id = id; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public String getTitle() { return title; }
+    public String getTitle() {
+        return title;
+    }
 
-  public void setTitle(String title) { this.title = title; }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
 }

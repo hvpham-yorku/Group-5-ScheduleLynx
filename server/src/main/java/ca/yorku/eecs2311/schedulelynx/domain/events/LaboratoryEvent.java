2256effa48c2eed3e6404d5a6c80b7c08a7ffa3b
Eric Hanson <ehanson@my.yorku.ca>
2026-02-22 22:29:55 -0500

    feat: Add `LaboratoryEvent` class to represent lab-specific events

---
+++ server/src/main/java/ca/yorku/eecs2311/schedulelynx/domain/events/LaboratoryEvent.java
@@ -0,0 +1,62 @@
+package ca.yorku.eecs2311.schedulelynx.domain.events;
+
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public class LaboratoryEvent extends AbstractEvent
+        implements StartableEvent, RecurrableEvent {
+
+    protected LocalDateTime start;
+    protected Recurrence recurrence;
+
+    public LaboratoryEvent() {}
+
+    public LaboratoryEvent(
+            @NotBlank long id,
+            @NotNull EventType type,
+            @NotNull  String name,
+            @Nullable String desc,
+            @NotNull LocalDateTime start,
+            @NotNull  LocalDateTime end,
+            @Nullable Recurrence recurrence,
+            @Nullable Difficulty difficulty)
+    {
+        super(id, type, name, desc, end, difficulty);
+
+        this.start = start;
+        this.recurrence = (recurrence != null) ? recurrence : Recurrence.NONE;
+
+        if (type == EventType.LABORATORY) return;
+        throw new IllegalArgumentException("""
+                Type != 'LABORATORY'.
+                Make sure you're calling the right class for:\s""" + type.name()
+        );
+    }
+
+    @Override
+    public LocalDateTime getStartDateTime() {
+
+        return start;
+    }
+
+    @Override
+    public void setStartDateTime(@NotNull LocalDateTime start) {
+
+        this.start = start;
+    }
+
+    @Override
+    public Recurrence getRecurrence() {
+
+        return recurrence;
+    }
+
+    @Override
+    public void setRecurrence(@Nullable Recurrence recurrence) {
+
+        this.recurrence = (recurrence != null) ? recurrence : Recurrence.NONE;
+    }
+}

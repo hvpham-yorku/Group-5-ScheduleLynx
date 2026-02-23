32e8cfcce3958ec583a3fb2ffee85651d2783786
Eric Hanson <ehanson@my.yorku.ca>
2026-02-22 23:17:20 -0500

    fix: Align the `EventResponse` class with the `EventRequest` class

--- server/src/main/java/ca/yorku/eecs2311/schedulelynx/web/dto/EventResponse.java
+++ server/src/main/java/ca/yorku/eecs2311/schedulelynx/web/dto/EventResponse.java
@@ -3,7 +3,6 @@ package ca.yorku.eecs2311.schedulelynx.web.dto;
 import ca.yorku.eecs2311.schedulelynx.domain.events.Difficulty;
 import ca.yorku.eecs2311.schedulelynx.domain.events.EventType;
 import ca.yorku.eecs2311.schedulelynx.domain.events.Recurrence;
-import ca.yorku.eecs2311.schedulelynx.domain.Weekday;
 import ca.yorku.eecs2311.schedulelynx.web.controller.EventController;
 
 import java.time.LocalDateTime;
@@ -13,13 +12,12 @@ import java.time.LocalDateTime;
  * @see EventController for other parameters' documentation.
  */
 public record EventResponse(
-        long id,
-        EventType type,
-        String title,
-        Weekday day,
-        LocalDateTime start,
-        LocalDateTime end,
-        Recurrence recurrence,
-        int estMinutes,
-        Difficulty difficulty
-) {}
+        long            id,
+        EventType       type,
+        String          name,
+        String          description,
+        LocalDateTime   start,
+        LocalDateTime   end,
+        Recurrence      recurrence,
+        int             estMinutes,
+        Difficulty      difficulty

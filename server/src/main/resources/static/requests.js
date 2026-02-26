1cac6375e89289f308bb44ebc47958e1b21ef810
Eric Hanson <ehanson@my.yorku.ca>
2026-02-26 02:29:00 -0500

    refactor: Move the function 'convertDateToWeekday' from the requests.js file to the new utils.js file so it can be reused elsewhere

--- server/src/main/resources/static/requests.js
+++ server/src/main/resources/static/requests.js
@@ -1,20 +1,11 @@
+import {convertDateToWeekday} from "./utils";
+
 const baseURL = "http://localhost:8080";
 
 
 // ================================
 // Calendar Item Creation Functions
 // ================================
-
-/** the date should be in the format of "YYYY-MM-DD."
- *  @return the day of the week's full name in all caps. */
-function convertDateToWeekday(date) {
-
-    const localDate = date.replaceAll('-', '/');
-    const dateObject = new Date(localDate);
-    const day = dateObject.toLocaleDateString("en-US", { weekday: "long" });
-
-    return day.toUpperCase();
-}
 
 /** Do NOT call this directly. Call postCalendarEvent() instead. */
 async function postCalendarEvent(dataObject) {
---
+++ server/src/main/resources/static/utils.js
@@ -0,0 +1,10 @@
+/** the date should be in the format of "YYYY-MM-DD."
+ *  @return the day of the week's full name in all caps. */
+export function convertDateToWeekday(date) {
+
+    const localDate = date.replaceAll('-', '/');
+    const dateObject = new Date(localDate);
+    const day = dateObject.toLocaleDateString("en-US", {weekday: "long"});
+
+    return day.toUpperCase();
+}

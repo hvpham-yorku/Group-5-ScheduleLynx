12a85e0ae47f734ea55635b58e490cd7cd410326
Mykola Bohomaz <bohomazmykola@gmail.com>
2026-03-01 02:02:38 -0500

    added user domain and repo

---
+++ server/src/main/java/ca/yorku/eecs2311/schedulelynx/domain/User.java
@@ -0,0 +1,19 @@
+package ca.yorku.eecs2311.schedulelynx.domain;
+
+public class User {
+  private final Long id;
+  private final String username;
+  private final String passwordHash;
+
+  public User(Long id, String username, String passwordHash) {
+    this.id = id;
+    this.username = username;
+    this.passwordHash = passwordHash;
+  }
+
+  public Long getId() { return id; }
+
+  public String getUsername() { return username; }
+
+  public String getPasswordHash() { return passwordHash; }
+}

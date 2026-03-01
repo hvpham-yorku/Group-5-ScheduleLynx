14b940f24471b743a91d3cf43746545a3ed92a81
Mykola Bohomaz <bohomazmykola@gmail.com>
2026-03-01 02:25:26 -0500

    fixed compilation error

--- server/src/main/java/ca/yorku/eecs2311/schedulelynx/web/dto/AuthRequests.java
+++ server/src/main/java/ca/yorku/eecs2311/schedulelynx/web/dto/AuthRequests.java
@@ -2,10 +2,8 @@ package ca.yorku.eecs2311.schedulelynx.web.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record RegisterRequest(@NotBlank String username,
-                              @NotBlank String password) {}
+record RegisterRequest(@NotBlank String username, @NotBlank String password) {}
 
-public record LoginRequest(@NotBlank String username,
-                           @NotBlank String password) {}
+record LoginRequest(@NotBlank String username, @NotBlank String password) {}
 
-public record MeResponse(Long id, String username) {}
+record MeResponse(Long id, String username) {}
